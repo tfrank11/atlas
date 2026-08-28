ea28e88ff9e933673cebaa9ef93173cce4c64b06
tfrank11 <timmyfrank3@gmail.com>
2026-08-28 01:59:31 +0000

    added worker login retry mechanism with exponential backoff

--- src/atlas/worker.py
+++ src/atlas/worker.py
@@ -1,7 +1,6 @@
 import asyncio
 
 import cloudpickle
-import msgpack
 
 from atlas.lib.protocol_utils import deserialize_header, read_frame, serialize_request
 from atlas.lib.types import (
@@ -14,10 +13,12 @@ from atlas.lib.types import (
 
 
 class Worker:
+    scheduler_reconnect_retries = 0
+    max_scheduler_reconnect_sec = 60
+
     async def handle_submit_task(
         self, writer: asyncio.StreamWriter, header: ClientSubmitTask, body: bytes
     ):
-        print(f"[Worker] received header={header}")
         print(f"[Worker] starting task with id={header.task_id}")
         busy_header = WorkerBusy(task_id=header.task_id)
         writer.write(serialize_request(busy_header))
@@ -65,17 +66,32 @@ class Worker:
         writer.close()
 
     async def start(self, scheduler_host: str, scheduler_port: int):
-        reader, writer = await asyncio.open_connection(
-            host=scheduler_host, port=scheduler_port
-        )
+        try:
+            reader, writer = await asyncio.open_connection(
+                host=scheduler_host, port=scheduler_port
+            )
 
-        worker_login_header = WorkerLogin()
-        payload = serialize_request(worker_login_header)
-        writer.write(payload)
-        print(f"[Worker] logging into scheduler at {scheduler_host}:{scheduler_port}")
-        await writer.drain()
+            worker_login_header = WorkerLogin()
+            payload = serialize_request(worker_login_header)
+            writer.write(payload)
+            print(
+                f"[Worker] logging into scheduler at {scheduler_host}:{scheduler_port}"
+            )
+            await writer.drain()
 
-        await self.handle(writer=writer, reader=reader)
+            await self.handle(writer=writer, reader=reader)
+            self.scheduler_reconnect_retries = 0
+        except Exception as e:  # noqa: BLE001
+            sec_until_retry = min(
+                self.max_scheduler_reconnect_sec,
+                (2**self.scheduler_reconnect_retries),
+            )
+            print(f"[Worker] error={e} retrying in {sec_until_retry} seconds")
+            await asyncio.sleep(delay=sec_until_retry)
+            self.scheduler_reconnect_retries += 1
+            await self.start(
+                scheduler_host=scheduler_host, scheduler_port=scheduler_port
+            )
 
 
 async def main():
