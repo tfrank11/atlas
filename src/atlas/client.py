41b344ebdd83b0eb313b17b140012662375b316b
tfrank11 <timmyfrank3@gmail.com>
2026-08-30 18:44:18 +0000

    refactor client code into class

--- src/atlas/client.py
+++ src/atlas/client.py
@@ -7,42 +7,66 @@ from atlas.lib.protocol_utils import deserialize_header, read_frame, serialize_r
 from atlas.lib.types import ClientSubmitTask, RequestType
 
 
-def send_task(writer: asyncio.StreamWriter, task_id: int, task_fn: callable):
-    header_obj = ClientSubmitTask(task_id=task_id)
-    payload = serialize_request(header_obj=header_obj, body=task_fn)
-    writer.write(payload)
+class Client:
+    scheduler_host: str
+    scheduler_port: int
+    debug: bool
+
+    def __init__(self, scheduler_host: str, scheduler_port: int, debug: bool = False):
+        self.scheduler_host = scheduler_host
+        self.scheduler_port = scheduler_port
+        self.debug = debug
+
+    async def run(self, fn: callable):
+        reader, writer = await asyncio.open_connection(
+            self.scheduler_host, self.scheduler_port
+        )
+        task_id = str(uuid4())
+        header_obj = ClientSubmitTask(task_id=task_id)
+        payload = serialize_request(header_obj=header_obj, body=fn)
+        writer.write(payload)
+
+        await writer.drain()
+
+        while True:
+            frame = await read_frame(reader)
+            if not frame:
+                break
+
+            header = deserialize_header(frame.header_bytes)
+            if header.type == RequestType.SCHEDULER_ACK_TASK:
+                if self.debug:
+                    print(f"[Client] received task ack from scheduler ({header.type})")
+            elif header.type == RequestType.WORKER_TASK_FINISHED:
+                if self.debug:
+                    print(f"[Client] scheduler finished task ({header.task_id})")
+                try:
+                    rtn = cloudpickle.loads(frame.body_bytes)
+                    if self.debug:
+                        print(f"[Client] task finished with return value: {rtn}")
+                    writer.close()
+                    await writer.wait_closed()
+                    return rtn
+                except Exception as e:  # noqa: BLE001
+                    print(f"[Client] error handling worker return value: {e}")
+                    return None
 
 
 async def main():
-    scheduler_host = "127.0.0.1"
-    scheduler_port = 8700
-    reader, writer = await asyncio.open_connection(scheduler_host, scheduler_port)
+    client = Client(scheduler_host="127.0.0.1", scheduler_port=8700)
 
-    def task_fn():
+    def task1():
         print("hello from client")
-        return 123
+        return 100
 
-    task_id = str(uuid4())
-    send_task(writer, task_id=task_id, task_fn=task_fn)
-    await writer.drain()
+    res1 = await client.run(task1)
+    print(f"res1={res1}")
 
-    while True:
-        frame = await read_frame(reader)
-        if not frame:
-            break
+    def task2():
+        return res1 * 2
 
-        header = deserialize_header(frame.header_bytes)
-        if header.type == RequestType.SCHEDULER_ACK_TASK:
-            print(f"[Client] received task ack from scheduler ({header.type})")
-        elif header.type == RequestType.WORKER_TASK_FINISHED:
-            print(f"[Client] scheduler finished task ({header.task_id})")
-            try:
-                rtn = cloudpickle.loads(frame.body_bytes)
-                print(f"[Client] task finished with return value: {rtn}")
-                writer.close()
-                await writer.wait_closed()
-            except Exception as e:  # noqa: BLE001
-                print(f"[Client] error handling worker return value: {e}")
+    res2 = await client.run(task2)
+    print(f"res2={res2}")
 
 
 asyncio.run(main())
